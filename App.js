c550fe6ee805504b1579f88d338ba506518db99c
NIRANJAN7975 <116199094+NIRANJAN7975@users.noreply.github.com>
2025-02-13 21:49:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Use 'Routes' instead of 'Switch'
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Projects from './pages/Project';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import './styles.css';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
